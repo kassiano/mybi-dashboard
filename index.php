fb955daffd9e69e7b84e33453980864d4cac8574
kassiano <kassiano.resende@gmail.com>
2019-11-11 20:34:36 -0300

    display error

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 ﻿
 <?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
     if(isset($_GET['usuario']){
         
         $usuario = $_GET["usuario"];
